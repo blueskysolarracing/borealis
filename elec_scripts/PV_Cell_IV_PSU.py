1be092f149b9a240904badb758f3a01019c295fa
Tristan <robitaille.tristan@gmail.com>
2022-05-08 10:22:05 -0400

    Added ability to display MPP when greeting user at script startup

--- elec_scripts/PV_Cell_IV_PSU.py
+++ elec_scripts/PV_Cell_IV_PSU.py
@@ -4,10 +4,31 @@ import DP800
 import pyvisa as pv
 
 init_current = 2.0 #Initial current to get initial operating point on the IV curve
+max_current = 6.0 #Max. allowed current from PSU (although DP800 can only reach 5A)
 
+#Methods
 def compVolt(curr):
     #Simulates 2 cells in series with I_sc of ~3.5A
     return (30.03*curr**3 - 56.22*curr**2 - 3436*curr + 11480) / (curr**2 - 2852*curr + 10170) #Fit from eve module, current increased
+
+def getMPP():
+    #Determines the maximum voltage and current (MPP) on the given IV curve
+    num_points = 10000
+    interval = max_current / num_points
+    powerList = [] #List of maxPowerList's
+    maxPowerList = [] #Carries current, voltage and power for each point
+
+    for i in range (num_points):
+        current = interval * i
+        voltage = compVolt(current)
+
+        newList = [current, voltage, current*voltage]
+        powerList.append(newList)
+
+        if newList[2] > maxPowerList[2]: #Update maximum power point
+            maxPowerList = newList
+
+    return maxPowerList
 
 #Open connection
 targetID = "USB0::6833::3601::DP8A234200572::0::INSTR"
@@ -28,11 +49,14 @@ PSU.setCURR(init_current) #2A
 PSU.setVOLT(compVolt(init_current)) #Set voltage corresponding to 2A
 PSU.channelON()           
 
+#Greet user
+MPP = getMPP()
+print("Welcome to the Solar Cell Simulator.\n Max. power is " + str(MPP[2]) "W (at " + str(MPP[1]) + "V, " + str(MPP[0]) + "A)")
+
 #Maintain IV curve
 while 1:
     #Read current
     i = PSU.measureValue("Current") #Measure current with 0.0s delay
-    print("Current: " + str(i))
-
+    
     #Set target voltage
     PSU.setVOLT(compVolt(i), 0.0) #Set voltage corresponding to current with 0.0s delay
