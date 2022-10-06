fac713c436d118a2d15770113392d62957766066
Raymond <raymond.yang121@outlook.com>
2022-10-06 19:50:26 -0400

    add MPPT code

---
+++ Master_Workspace/mppt/types.h
@@ -0,0 +1,28 @@
+/********************************************************************************************************
+USER LICENCE AGREEMENT and DISCLAIMER:
+This source code is the property of NanoGrid Ltd.
+Any modification made to this source code remains the property of NanoGrid Ltd.
+The source code may only be used for specific authorized peak powers.
+The source code is confidential and must be protected and must not be distributed.
+NanoGrid Ltd assumes no liability for damage or harm or losses caused by this source code.
+Deviation from this agreement requires permission from NanoGrid Ltd.
+This agreement must appear at the top of each source file and applies to the source as well
+ as accompanying binary, object, library, or any related files.
+Cantact: Tom Rodinger, tom@nanoleaf.me
+********************************************************************************************************/
+
+#include <p24hj64gp502.h>
+
+#ifndef __types
+#define __types
+
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef int int16;
+typedef unsigned int uint16;
+typedef unsigned char int8;
+typedef char uint8;
+#define U1 ((uint32)1)
+
+#endif
+
