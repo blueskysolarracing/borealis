ffe7bb37da1e12ca2a72e5806692407f115e3547
Rishabh Iyer <rishabh.iyer2050@gmail.com>
2023-10-08 01:48:44 +0930

    SWB Encoder Code

--- Master_Workspace/GEN11_SWB/Core/Src/stm32l4xx_it.c
+++ Master_Workspace/GEN11_SWB/Core/Src/stm32l4xx_it.c
@@ -201,6 +201,21 @@ void SysTick_Handler(void)
 /******************************************************************************/
 
 /**
+  * @brief This function handles EXTI line[9:5] interrupts.
+  */
+void EXTI9_5_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI9_5_IRQn 0 */
+
+  /* USER CODE END EXTI9_5_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_6);
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_7);
+  /* USER CODE BEGIN EXTI9_5_IRQn 1 */
+
+  /* USER CODE END EXTI9_5_IRQn 1 */
+}
+
+/**
   * @brief This function handles TIM6 global interrupt, DAC channel1 and channel2 underrun error interrupts.
   */
 void TIM6_DAC_IRQHandler(void)
