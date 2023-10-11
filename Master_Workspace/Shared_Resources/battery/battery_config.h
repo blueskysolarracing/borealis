6e3d2b33a72a55a3406ade31e20164de699e4544
Raymond <raymond.yang121@outlook.com>
2023-10-11 22:31:40 +0930

    increase OT and OV threshold

--- Master_Workspace/Shared_Resources/battery/battery_config.h
+++ Master_Workspace/Shared_Resources/battery/battery_config.h
@@ -2,7 +2,7 @@
 #define __BATTERY_CONFIG_H__
 
 #define HV_BATT_OC_THRESHOLD 	(60.0f) 	//Should be set to 60.0A
-#define HV_BATT_OV_THRESHOLD 	(4.20f)	    //Should be set to 4.20V
+#define HV_BATT_OV_THRESHOLD 	(4.25f)	    //Should be set to 4.25V
 #define HV_BATT_UV_THRESHOLD 	(2.50f)	    //Should be set to 2.50V
 #define HV_BATT_OT_THRESHOLD 	(60.0f) 	//Should be set to 60.0C
 #define HV_BATT_UT_THRESHOLD 	(0.0f) 		//Should be set to 0.0C
@@ -21,7 +21,7 @@
 #define BATTERY_SOC_INITIAL_VALUE               (-1000.0f)
 
 #define MAX_ALLOWD_OVERCURRENT_TIME (7000) // 7 seconds
-#define MOTOR_OT_THRESHOLD (120.0f) // 120.0C
+#define MOTOR_OT_THRESHOLD (180.0f) // 180.0C
 #define MOTOR_OT_WARNING_THRESHOLD (100.0f) // 100.0C
 
 #define HV_BATT_OV_DEBOUNCE_TIME 3000 // 3 seconds
